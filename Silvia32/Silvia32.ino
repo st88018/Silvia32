935bd082acf744abb5776d785844aaf50ca0b7db
Jeremy Chang <37570145+st88018@users.noreply.github.com>
2023-01-17 18:48:37 +0800

    Update Silvia32.ino

--- Silvia32/Silvia32.ino
+++ Silvia32/Silvia32.ino
@@ -32,11 +32,12 @@ float currentWeight;
 float currentPressure;
 
 // Mode management
-int mode = 1;  // Mode 0: Heating, 1: Brew, 2: Setting;
+int mode = 1;  // Mode 0: Heating, 1: Brew, 2: Brewing, 3: Clean, 4: Setting;
 int cursurPos = 0;
 
 bool brewstarted = 0;
-float brewpercent;
+float brewpercent = 0; //something from 0~1
+int cleancount = 0;
 
 //Blink
 int blinkcounter = 0;
@@ -54,9 +55,8 @@ void setup() {
 
   oled_initialize();
 
-  //collect eeprom data
+  /*collect eeprom data*/
   EEPROM.begin(128);
-
   // EEPROM.write(pressureAddr, targetpressure);
   // EEPROM.write(weightAddr, targetWeight);
   // EEPROM.write(preinfusionAddr, targetpreinfusion);
@@ -68,7 +68,7 @@ void setup() {
   targetpreinfusion = EEPROM.read(preinfusionAddr);
   Serial.print("EEPROM updated");
 
-  //Initialize manual input
+  /*Initialize manual input*/
   presstime = millis();
   attachInterrupt(digitalPinToInterrupt(6), pressup, FALLING);
   attachInterrupt(digitalPinToInterrupt(7), pressmid, FALLING);
@@ -82,6 +82,7 @@ void setup() {
 
 void Core0code(void* pvParameters) {
   for (;;) {
+    
     // if (millis() - code0timer > 1000) {
       // code0timer = millis();
       serial_debug();
@@ -580,8 +581,8 @@ void oled_initialize() {
 }
 
 void oled_display() {
-  if (mode == 0 && millis() > 5000) {
-    display.clearDisplay();
+  display.clearDisplay();
+  if (mode == 0 && millis() > 4000) {
     display.setTextSize(2);
     display.setTextColor(SH110X_WHITE);
     display.setCursor(25, 10);
@@ -599,16 +600,33 @@ void oled_display() {
     if (currentTemp > 80) mode = 1;
   }
   if (mode == 1) {
-    display.clearDisplay();
-    displayStatuscolumn();
     displayTemp();
     displayPress();
     displayWeight();
     displayPreinfusion();
-    display.display();
   }
+  if(mode == 2){
+    displayTemp();
+    displayPress();
+    displayWeight();
+    displayPreinfusion();
+  }if(mode ==3){
+    display.drawRect(0, 0, 128, 56, SH110X_WHITE);
+    display.setTextSize(1);
+    display.setTextColor(SH110X_WHITE);
+    display.setCursor(8, 15);
+    display.print("Press Brew to start");
+    display.setTextSize(2);
+    display.setTextColor(SH110X_WHITE);
+    display.setCursor(80, 32);
+    display.print(cleancount);
+  }if(mode ==4){
+    display.drawRect(0, 0, 128, 56, SH110X_WHITE);
+    display.setTextSize(1);
+  }
+  displayStatuscolumn();
+  display.display();
 }
-
 void userinterface() {
   if (mode == 0) {
     if (pressedup) {
@@ -626,71 +644,111 @@ void userinterface() {
       Serial.println("TempSaved");
     }
   }
+  if (mode == 1){
+    if(pressedmid){
+      pressedmid = false;
+      cursurPos++;
+      if(cursurPos>3) cursurPos = 1;
+    }
+    if (millis() - presstime > 10000){ //stop selecting while not using
+      cursurPos = 0;
+    }
+  }
 }
-
 void serial_debug() {
   Serial.print("Mode:");
   Serial.print(mode);
+  Serial.print(" cursurPos:");
+  Serial.print(cursurPos);
   Serial.print(" TargetTemp:");
   Serial.print(targetTemp);
   Serial.print(" CurrentTemp:");
   Serial.println(currentTemp);
+  Serial.println(blinkstate);
 }
 void displayStatuscolumn(){
   int BrewPos[2] = {8,56};
   int cleanPos[2] = {41,56};
   int settingPos[2] = {80,56};
+  display.setTextSize(1);
   if(mode == 1){ // Wait for Brew
-    display.setTextSize(1);
-    
     display.fillTriangle(0,55, 4,55, 4,64, SH110X_WHITE);
     display.fillRect(5,55,3,9, SH110X_WHITE);
-    display.fillTriangle(34,64, 34,55, 38,55, SH110X_WHITE);
-    display.fillRect(32,55,2,9, SH110X_WHITE);
-
-    // display.drawLine(0,55, 5,64,SH110X_WHITE);
-    
     display.setTextColor(SH110X_BLACK, SH110X_WHITE);
-    // display.setTextColor(SH110X_WHITE);
     display.setCursor(BrewPos[0],BrewPos[1]);
     display.print("Brew");
-
-    // display.drawLine(cleanPos[0]-8,64, cleanPos[0]-3,55, SH110X_WHITE);
-    
+    display.fillRect(32,55,2,9, SH110X_WHITE);
+    display.fillTriangle(34,64, 34,55, 38,55, SH110X_WHITE);
     display.setTextColor(SH110X_WHITE);
     display.setCursor(cleanPos[0], cleanPos[1]);
     display.print("clean");
-    
     display.drawLine(settingPos[0]-8,64, settingPos[0]-3,55, SH110X_WHITE);
-    
     display.setCursor(settingPos[0], settingPos[1]);
     display.print("setting");
-    
     display.drawLine(123,64, 128,55, SH110X_WHITE);
   }
   if(mode == 2){ //Brewing
-
+    display.fillTriangle(0,55, 4,55, 4,64, SH110X_WHITE);
+    display.fillRect(5,55,3,9, SH110X_WHITE);
+    display.setTextColor(SH110X_BLACK, SH110X_WHITE);
+    display.setCursor(BrewPos[0],BrewPos[1]);
+    display.print("Brew");
+    display.fillRect(32,55,2,9, SH110X_WHITE);
+    display.fillTriangle(34,64, 34,55, 38,55, SH110X_WHITE);
+    displaybrewstate();
   }
   if(mode == 3){ //Clean
-
-
+    display.setTextColor(SH110X_WHITE);
+    display.drawLine(0,55, 5,64,SH110X_WHITE);
+    display.setCursor(BrewPos[0],BrewPos[1]);
+    display.print("Brew");
+    display.fillTriangle(34,55, 38,55, 38,64, SH110X_WHITE);
+    display.fillRect(38,55,3,9, SH110X_WHITE);
+    display.setTextColor(SH110X_BLACK, SH110X_WHITE);
+    display.setCursor(cleanPos[0], cleanPos[1]);
+    display.print("clean");
+    display.fillRect(70,55,2,9, SH110X_WHITE);
+    display.fillTriangle(72,64, 72,55, 76,55, SH110X_WHITE);
+    display.setTextColor(SH110X_WHITE);
+    display.setCursor(settingPos[0], settingPos[1]);
+    display.print("setting");
+    display.drawLine(123,64, 128,55, SH110X_WHITE);
   }
   if(mode == 4){ //Setting
-
+    display.setTextColor(SH110X_WHITE);
+    display.drawLine(0,55, 5,64,SH110X_WHITE);
+    display.setCursor(BrewPos[0],BrewPos[1]);
+    display.print("Brew");
+    display.drawLine(cleanPos[0]-8,55, cleanPos[0]-3,64, SH110X_WHITE);
+    display.setCursor(cleanPos[0], cleanPos[1]);
+    display.print("clean");
+    display.fillTriangle(73,55, 77,55, 77,64, SH110X_WHITE);
+    display.fillRect(77,55,3,9, SH110X_WHITE);
+    display.setTextColor(SH110X_BLACK, SH110X_WHITE);
+    display.setCursor(settingPos[0], settingPos[1]);
+    display.print("setting");
+    display.fillRect(122,55,1,9, SH110X_WHITE);
+    display.fillTriangle(123,64, 123,55, 127,55, SH110X_WHITE);
   }
-  
 }
-void displaybrewstate(float brewpercent) {  //0 to 1
-  int i = brewpercent * 10;                                   //shoud be 1~10
-  int j;
-  if (i < 1) {
-    if(blinkstate) i = 1;
-  }
-  for (j = 0; j < 10 - i; j++) {
-    display.drawRect(119, j * 6 + 2, 9, 5, SH110X_WHITE);
-  }
-  for (j; j < 10; j++) {
-    display.fillRect(119, j * 6 + 2, 9, 5, SH110X_WHITE);
+void displaybrewstate() {  //0 to 1
+  display.drawLine(34,63, 123,63, SH110X_WHITE);
+  display.drawLine(123,64, 128,55, SH110X_WHITE);
+  int i;
+  if(brewpercent == 0){
+    if(blinkstate){
+      i = 0.05*86;
+      display.fillRect(35,55,i,9, SH110X_WHITE); //35~121
+      display.fillTriangle(i+35,64, i+35,55, i+39,55, SH110X_WHITE);
+    }else{
+      i = 0;
+      display.fillRect(35,55,i,9, SH110X_WHITE); //35~121
+      display.fillTriangle(i+35,64, i+35,55, i+39,55, SH110X_WHITE);
+    }
+  }else{
+    i = brewpercent*86;
+    display.fillRect(35,55,i,9, SH110X_WHITE); //35~121
+    display.fillTriangle(i+35,64, i+35,55, i+39,55, SH110X_WHITE);
   }
 }
 void displayTemp() {
@@ -700,7 +758,12 @@ void displayTemp() {
   display.setCursor(2, 2);
   display.print("Temp");
   display.print(" (");
+  if(cursurPos == 1){
+    display.drawLine(37,2,37,9, SH110X_WHITE);
+    display.setTextColor(SH110X_BLACK, SH110X_WHITE);
+  }
   display.print(targetTemp);
+  display.setTextColor(SH110X_WHITE);
   display.print(")");
   display.setCursor(15, 12);
   display.setTextSize(2);
@@ -723,7 +786,12 @@ void displayWeight() {
   display.setCursor(66, 2);
   display.print("Weight");
   display.print("(");
+  if(cursurPos == 2){
+    display.drawLine(107,2,107,9, SH110X_WHITE);
+    display.setTextColor(SH110X_BLACK, SH110X_WHITE);
+  }
   display.print(targetWeight);
+  display.setTextColor(SH110X_WHITE);
   display.print(")");
   display.setTextSize(2);
   display.setCursor(79, 12);
@@ -733,11 +801,14 @@ void displayPreinfusion() {
   display.drawRect(64, 28, 64, 28, SH110X_WHITE);
   display.setTextSize(1);
   display.setTextColor(SH110X_WHITE);  // Draw white text
-  // display.setTextColor(SH110X_BLACK, SH110X_WHITE);
   display.setCursor(66, 30);
   display.print("Preinfuse");
   display.setTextSize(2);
   display.setCursor(79, 40);
+  if(cursurPos == 3){
+    display.drawLine(79,40,79,54, SH110X_WHITE);
+    display.setTextColor(SH110X_BLACK, SH110X_WHITE);
+  }
   display.print(targetpreinfusion);
 }
 void blinkcontroller(){
